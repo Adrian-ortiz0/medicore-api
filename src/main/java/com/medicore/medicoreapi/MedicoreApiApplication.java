be512753970bb89ed29b29cd68b303f840c20332
Adrian-ortiz0 <dxniel7328@gmail.com>
2026-03-13 07:55:21 -0500

    Initial project setup

---
+++ src/main/java/com/medicore/medicoreapi/MedicoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.medicore.medicoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicoreApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicoreApiApplication.class, args);
+    }
+
+}
